350aeec5075762b86ca141ccf3e394d3468f1c2f
dimnov <dimnov1512@gmail.com>
2024-05-13 00:09:29 +0300

    Add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Homepage from "./pages/Homepage.jsx";
+import Product from "./pages/Product.jsx";
+import Pricing from "./pages/Pricing.jsx";
+import PageNotFound from "./pages/PageNotFound.jsx";
+import Login from "./pages/Login.jsx";
+
 function App() {
-  return <div>hello</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
