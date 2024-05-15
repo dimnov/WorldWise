f2a1b2f08c9b07946fd85ee0665cc1dd1d6a9773
dimnov <dimnov1512@gmail.com>
2024-05-15 21:16:10 +0300

    Add AppLayout component

---
+++ src/pages/AppLayout/AppLayout.jsx
@@ -0,0 +1,14 @@
+import Map from "../../components/Map/Map.jsx";
+import Sidebar from "../../components/Sidebar/Sidebar.jsx";
+import styles from "./AppLayout.module.css";
+
+function AppLayout() {
+  return (
+    <div className={styles.app}>
+      <Sidebar />
+      <Map />
+    </div>
+  );
+}
+
+export default AppLayout;
