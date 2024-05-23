ccb249b9d7d7ab7bbbe168c4ee3a565e0aeadd50
dimnov <dimnov1512@gmail.com>
2024-05-23 16:42:34 +0300

    Add route guard

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import City from "./components/City/City.jsx";
 import Form from "./components/Form/Form.jsx";
 import { CitiesProvider } from "./contexts/CitiesContext.jsx";
 import { AuthProvider } from "./contexts/FakeAuthContext.jsx";
+import ProtectedRoute from "./pages/ProtectedRoute.jsx";
 
 function App() {
   return (
@@ -22,7 +23,14 @@ function App() {
             <Route path="/login" element={<Login />} />
             <Route path="product" element={<Product />} />
             <Route path="pricing" element={<Pricing />} />
-            <Route path="app" element={<AppLayout />}>
+            <Route
+              path="app"
+              element={
+                <ProtectedRoute>
+                  <AppLayout />
+                </ProtectedRoute>
+              }
+            >
               <Route index element={<Navigate replace to={"cities"} />} />
               <Route path="cities" element={<CityList />} />
               <Route path="cities/:id" element={<City />} />
