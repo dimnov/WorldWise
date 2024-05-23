fe789287ff1e3d8ea2e950594d5c0d40fd8469b7
dimnov <dimnov1512@gmail.com>
2024-05-23 16:28:39 +0300

    Testing authentication

--- src/pages/AppLayout/AppLayout.jsx
+++ src/pages/AppLayout/AppLayout.jsx
@@ -1,11 +1,13 @@
 import Map from "../../components/Map/Map.jsx";
 import Sidebar from "../../components/Sidebar/Sidebar.jsx";
+import User from "../../components/User/User.jsx";
 import styles from "./AppLayout.module.css";
 
 function AppLayout() {
   return (
     <div className={styles.app}>
       <Sidebar />
+      <User />
       <Map />
     </div>
   );
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,16 +1,35 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./Login.module.css";
 import PageNav from "../../components/PageNav/PageNav.jsx";
+import { useAuth } from "../../contexts/FakeAuthContext.jsx";
+import { useNavigate } from "react-router-dom";
+import Button from "../../components/Button/Button.jsx";
 
 export default function Login() {
   // PRE-FILL FOR DEV PURPOSES
   const [email, setEmail] = useState("jack@example.com");
   const [password, setPassword] = useState("qwerty");
 
+  const { isAuthenticated, login } = useAuth();
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigate("/app", { replace: true });
+    }
+  }, [isAuthenticated, navigate]);
+
+  function handleLogin(e) {
+    e.preventDefault();
+
+    if (email && password) login(email, password);
+  }
+
   return (
     <main className={styles.login}>
       <PageNav />
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={handleLogin}>
         <div className={styles.row}>
           <label htmlFor="email">Email address</label>
           <input type="email" id="email" onChange={(e) => setEmail(e.target.value)} value={email} />
@@ -27,7 +46,7 @@ export default function Login() {
         </div>
 
         <div>
-          <button>Login</button>
+          <Button type={"primary"}>Login</Button>
         </div>
       </form>
     </main>
